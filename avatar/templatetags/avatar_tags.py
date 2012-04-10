01e939ba261f6e7f9c53ecac9aa002170e0cb908
Mark Liu <markwayneliu@gmail.com>
2012-04-10 06:58:54 -0500

    Added an option for fetching the default gravatar over https instead of http.

--- avatar/templatetags/avatar_tags.py
+++ avatar/templatetags/avatar_tags.py
@@ -8,7 +8,7 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 
 from avatar.settings import (AVATAR_GRAVATAR_BACKUP, AVATAR_GRAVATAR_DEFAULT,
-                             AVATAR_DEFAULT_SIZE)
+                             AVATAR_GRAVATAR_SSL, AVATAR_DEFAULT_SIZE)
 from avatar.util import get_primary_avatar, get_default_avatar_url, cache_result
 
 register = template.Library()
@@ -25,7 +25,12 @@ def avatar_url(user, size=AVATAR_DEFAULT_SIZE):
             params = {'s': str(size)}
             if AVATAR_GRAVATAR_DEFAULT:
                 params['d'] = AVATAR_GRAVATAR_DEFAULT
-            return "http://www.gravatar.com/avatar/%s/?%s" % (
+            if AVATAR_GRAVATAR_SSL:
+                prefix = 'https://secure'
+            else:
+                prefix = 'http://www'
+            return "%s.gravatar.com/avatar/%s/?%s" % (
+                prefix,
                 md5_constructor(user.email).hexdigest(),
                 urllib.urlencode(params))
         else:
